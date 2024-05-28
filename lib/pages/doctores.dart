a0d257dc53bb84efddc47db17357d7f438ed273f
Itzelipad <20690120@tecvalles.mx>
2024-05-27 19:46:24 -0600

    Diseño de doctores

--- lib/pages/doctores.dart
+++ lib/pages/doctores.dart
@@ -1,43 +1,44 @@
 import 'package:flutter/material.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 import 'cita.dart';
 
 class DoctoresPage extends StatelessWidget {
   final List<Map<String, dynamic>> doctors = [
     {
-      'nombre': 'Dr. Gregory House',
-      'especialidad': 'Nefrología e infectología',
-      'foto': const AssetImage('assets/house.jpg'),
+      'name': 'Dr. Alejandro',
+      'specialty': 'Pediatria',
+      'image':
+          'https://th.bing.com/th/id/OIP.IVwf85npYYUcwRp4EIhqDgAAAA?w=137&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7', // URL de la imagen del doctor
     },
     {
-      'nombre': 'Dra. Cameron',
-      'especialidad': 'Inmunología',
-      'foto': const AssetImage('assets/cameron.jpg'),
+      'name': 'Dra. María',
+      'specialty': 'Cardiología',
+      'image':
+          'https://th.bing.com/th/id/OIP.JODdTKvCi-ivu28zDmQk8gHaEj?w=289&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7', // URL de la imagen del doctor
     },
     {
-      'nombre': 'Dr. Chase',
-      'especialidad': 'Cardiología y cuidados intensivos',
-      'foto': const AssetImage('assets/chasse.jpg'),
+      'name': 'Dr. Juan',
+      'specialty': 'Dermatología',
+      'image':
+          'https://www.crhoy.com/wp-content/uploads/2019/01/Captura-de-pantalla-2019-01-03-a-las-10.48.27.png',
     },
     {
-      'nombre': 'Dr. Foreman',
-      'especialidad': 'Neurología',
-      'foto': const AssetImage('assets/Foreman.jpg'),
+      'name': 'Dr. Hector',
+      'specialty': 'Pediatria',
+      'image':
+      'https://th.bing.com/th/id/OIP.IVwf85npYYUcwRp4EIhqDgAAAA?w=137&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7', // URL de la imagen del doctor
     },
     {
-      'nombre': 'Dr. James Wilson',
-      'especialidad': 'Oncología',
-      'foto': const AssetImage('assets/james.jpeg'),
+      'name': 'Dra. Elena',
+      'specialty': 'Cardiología',
+      'image':
+      'https://th.bing.com/th/id/OIP.JODdTKvCi-ivu28zDmQk8gHaEj?w=289&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7', // URL de la imagen del doctor
     },
     {
-      'nombre': 'Dr. Christopher Taub',
-      'especialidad': 'Cirujano Plástico y Reconstructivo',
-      'foto': const AssetImage('assets/tod.jpeg'),
-    },
-    {
-      'nombre': 'Dra. Lisa Cuddy',
-      'especialidad': 'Endocrinología',
-      'foto': const AssetImage('assets/cuddy.jpeg'),
+      'name': 'Dr. Diego',
+      'specialty': 'Dermatología',
+      'image':
+      'https://www.crhoy.com/wp-content/uploads/2019/01/Captura-de-pantalla-2019-01-03-a-las-10.48.27.png',
     },
   ];
 
@@ -47,109 +48,124 @@ class DoctoresPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'Doctores',
-          style: TextStyle(color: Colors.blue),
+        title: Text(
+          "Doctores",
+          style: GoogleFonts.openSans(
+            color: const Color(0xFF0B8FAC),
+            fontSize: 27,
+            fontWeight: FontWeight.bold,
+          ),
         ),
+        centerTitle: true,
       ),
-      body: Padding(
+      body: SingleChildScrollView(
         padding: const EdgeInsets.all(10.0),
         child: Column(
           children: [
             const TextField(
+              keyboardType: TextInputType.text,
               decoration: InputDecoration(
-                labelText: 'Buscar Doctor',
-                border: OutlineInputBorder(),
                 prefixIcon: Icon(Icons.search),
+                labelText: "Buscar",
+                filled: true,
+                fillColor: Color(0x40D9D9D9),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  borderSide: BorderSide.none,
+                ),
+                floatingLabelBehavior: FloatingLabelBehavior.never,
               ),
             ),
             const SizedBox(height: 10),
-            Expanded(
-              child: ListView.builder(
-                itemCount: doctors.length,
-                itemBuilder: (context, index) {
-                  final doctor = doctors[index];
-                  return DoctorCard(
-                    nombre: doctor['nombre'],
-                    especialidad: doctor['especialidad'],
-                    foto: doctor['foto'],
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class DoctorCard extends StatelessWidget {
-  final String nombre;
-  final String especialidad;
-  final ImageProvider<Object> foto;
-
-  const DoctorCard({super.key,
-    required this.nombre,
-    required this.especialidad,
-    required this.foto,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(vertical: 10),
-      child: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Row(
-          children: [
-            SizedBox(
-              width: 80,
-              height: 80,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(8),
-                child: Image(
-                  image: foto,
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            const SizedBox(width: 15),
-            Expanded(
+            Padding(
+              padding: const EdgeInsets.only(left: 20, top: 10, right: 20),
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    nombre,
-                    style: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
+                children: doctors.map((doctor) {
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 10),
+                    child: Container(
+                      height: 200,
+                      decoration: BoxDecoration(
+                        color: const Color.fromRGBO(210, 235, 231, .3),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(left: 5),
+                            child: Container(
+                              width: 150,
+                              height: 190,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                image: DecorationImage(
+                                  image: NetworkImage(doctor['image'] ?? ''),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 20),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const SizedBox(height: 30),
+                                Text(
+                                  doctor['name'] ?? '',
+                                  style: GoogleFonts.openSans(
+                                    fontSize: 17,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                const SizedBox(height: 15),
+                                Text(
+                                  "Especialidad:",
+                                  style: GoogleFonts.openSans(
+                                    fontSize: 17,
+                                    fontWeight: FontWeight.bold,
+                                    color: const Color.fromRGBO(133, 133, 133, 1),
+                                  ),
+                                ),
+                                Text(
+                                  "${doctor['specialty']}",
+                                  style: GoogleFonts.openSans(
+                                    fontSize: 17,
+                                    color: const Color.fromRGBO(133, 133, 133, 1),
+                                  ),
+                                ),
+                                const SizedBox(height: 15),
+                                ElevatedButton(
+                                  onPressed: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => cita()),
+                                    );
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor:
+                                        const Color.fromRGBO(11, 143, 172, 1),
+                                    minimumSize: const Size(20, 30),
+                                  ),
+                                  child: const Text(
+                                    "Ver",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                                const SizedBox(height: 15)
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 5),
-                  Text(
-                    especialidad,
-                    style: const TextStyle(
-                      fontSize: 16,
-                    ),
-                  ),
-                ],
+                  );
+                }).toList(),
               ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => cita()),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.white,
-                backgroundColor: Colors.blue,
-              ),
-              child: const Text('Ver'),
-            ),
+            )
           ],
         ),
       ),
